5a75c663dc3ea1a43705c1e0c9f6696d7c1fec72
Tara Rhoseyn <taragulwell@gmail.com>
2022-01-11 21:39:02 +0000

    Add friendly_name key to productgroup and producttype models

--- products/models.py
+++ products/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class ProductGroup(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
+
 
     def __str__(self):
         return self.name
@@ -10,6 +12,7 @@ class ProductGroup(models.Model):
 
 class ProductType(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.name
