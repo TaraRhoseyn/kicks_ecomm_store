38622cf2322f146883bfb1aa3af3d8c46edc8e37
Tara Rhoseyn <taragulwell@gmail.com>
2022-01-12 20:28:29 +0000

    Add model meta info to products

--- products/models.py
+++ products/models.py
@@ -3,20 +3,38 @@ from django.core.validators import MaxValueValidator
 
 
 class ProductGroup(models.Model):
+
+
+    class Meta:
+        verbose_name_plural = 'ProductGroups'
+
     name = models.CharField(max_length=200, null=True, blank=True)
     friendly_name = models.CharField(max_length=200, null=True, blank=True)
 
 
     def __str__(self):
         return self.name
+
+
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 class ProductType(models.Model):
+
+
+    class Meta:
+        verbose_name_plural = 'ProductTypes'
+
     name = models.CharField(max_length=200, null=True, blank=True)
     friendly_name = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 class Product(models.Model):
@@ -24,15 +42,16 @@ class Product(models.Model):
     name = models.CharField(max_length=20, null=False, blank=False)
     description = models.TextField()
     product_group = models.ForeignKey(
-        'ProductGroup', null=True, blank=True, on_delete=models.SET_NULL)
+        'ProductGroup', default=1, null=True, blank=True, on_delete=models.SET_NULL)
     product_type = models.ForeignKey(
-        'ProductType', null=True, blank=True, on_delete=models.SET_NULL)
+        'ProductType', default=1, null=True, blank=True, on_delete=models.SET_NULL)
     price = models.DecimalField(
         max_digits=7, decimal_places=2, null=False, blank=False)
     rating = models.IntegerField(
         null=False, blank=False, validators=[MaxValueValidator(999)])
     brand = models.CharField(max_length=50, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
 
     def __str__(self):
         return self.name
