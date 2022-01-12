4163f50f7b656602d89dc62df16780c010742b99
Tara Rhoseyn <taragulwell@gmail.com>
2022-01-12 19:15:48 +0000

    Register product models with admin

--- products/admin.py
+++ products/admin.py
@@ -1,12 +1,35 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductGroup, ProductType
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'name',
         'description',
-        'completed',
+        'product_group',
+        'product_type',
+        'price',
+        'rating',
+        'brand',
+        'image'
+    )
+
+    ordering = ('sku',)
+
+
+class ProductGroupAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
     )
 
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductGroup, ProductGroupAdmin)
+admin.site.register(ProductType, ProductTypeAdmin)
