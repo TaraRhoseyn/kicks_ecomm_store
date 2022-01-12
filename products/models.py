30bbfc43a25a3689010dd0d2d784ce9c775558f2
Tara Rhoseyn <taragulwell@gmail.com>
2022-01-12 19:16:49 +0000

    fix: Fix max_digits and validators attributes on vars in product models

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 
 class ProductGroup(models.Model):
@@ -22,11 +23,14 @@ class Product(models.Model):
     sku = models.CharField(max_length=12, null=False, blank=False)
     name = models.CharField(max_length=20, null=False, blank=False)
     description = models.TextField()
-    product_group = models.ForeignKey('ProductGroup', null=True, blank=True)
-    product_type = models.ForeignKey('ProductType', null=True, blank=True)
+    product_group = models.ForeignKey(
+        'ProductGroup', null=True, blank=True, on_delete=models.SET_NULL)
+    product_type = models.ForeignKey(
+        'ProductType', null=True, blank=True, on_delete=models.SET_NULL)
     price = models.DecimalField(
         max_digits=7, decimal_places=2, null=False, blank=False)
-    rating = models.IntegerField(max_digitals=1, null=False, blank=False)
+    rating = models.IntegerField(
+        null=False, blank=False, validators=[MaxValueValidator(999)])
     brand = models.CharField(max_length=50, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
