1b31df129925f2da16c3773deca7a20c059f1305
Tara Rhoseyn <taragulwell@gmail.com>
2022-01-19 15:11:19 +0000

    Configure static media for local development

--- kicks/settings.py
+++ kicks/settings.py
@@ -149,9 +149,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    'static/',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
