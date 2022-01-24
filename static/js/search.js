13bdeeade2d0ceeb38ef03ea08215e7d45a16749
Tara Rhoseyn <taragulwell@gmail.com>
2022-01-24 16:45:13 +0000

    fix: Adjust method of displaying search results so search_term can be correctly displayed when no search results are available

--- static/js/search.js
+++ static/js/search.js
@@ -15,7 +15,12 @@ function checkSearchResult() {
 
 function displayNoResult() {
     searchResult.innerHTML = "No";
-    
+    var tryAgain = document.getElementById('try-again');
+    tryAgain.innerHTML = "Try searching another term.";
+    var redirect = document.getElementById('research-btn');
+    redirect.innerHTML = `<i class="fas fa-chevron-left"></i>  Keep shopping`;
+    tryAgain.classList.add("mb-3");
+
 }
 
 function displayResultNum() {
