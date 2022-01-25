33036236f2fc104c8f455f4760b0c17bc44712f6
Tara Rhoseyn <taragulwell@gmail.com>
2022-01-25 16:57:19 +0000

    refactor: Reduce javascript search logic by using Django's built-in length filter in template

--- static/js/search.js
+++ static/js/search.js
@@ -1,11 +1,9 @@
-const searchResult = document.getElementById('search-result-display');
-
 // Checks search results page to see whether there were any results
 function checkSearchResult() {
     var url = window.location.href;
     if (url.includes("search")) {
         if (document.getElementById('search-result')) {
-            displayResultNum();
+            return;
         } else {
             displayNoResult();
         }
@@ -16,21 +14,11 @@ function checkSearchResult() {
 
 // Gives feedback that no search results were found, directs to products.html
 function displayNoResult() {
-    searchResult.innerHTML = "No";
     var tryAgain = document.getElementById('try-again');
     tryAgain.innerHTML = "Try searching another term.";
     var redirect = document.getElementById('research-btn');
     redirect.innerHTML = `<i class="fas fa-chevron-left"></i>  Keep shopping`;
     tryAgain.classList.add("mb-3");
-
-}
-
-// Gives feedback of how many search results were found
-function displayResultNum() {
-    var results = document.getElementsByClassName('card-deck');
-    var resultsNum = results.length;
-    console.log(resultsNum);
-    searchResult.innerHTML = resultsNum;
 
 }
 
