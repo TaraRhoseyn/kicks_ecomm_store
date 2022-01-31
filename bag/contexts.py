3bc1aa96a884df6401019b8d854253e570562001
Tara Rhoseyn <taragulwell@gmail.com>
2022-01-31 22:04:25 +0000

    feat: Add to bag view and url created

--- bag/contexts.py
+++ bag/contexts.py
@@ -4,7 +4,7 @@ from decimal import Decimal
 # Makes dict available to all apps
 def bag_items(request):
 
-    bag_item_li = []
+    # bag_item_li = []
     total = 0
     product_nu = 0
     grand_total = 0
@@ -17,10 +17,10 @@ def bag_items(request):
         # insert code to let user know how much more to spend to get free deliv
 
     context = {
-        'bag_item_li': bag_items_li,
+        # 'bag_item_li': bag_items_li,
         'total': total,
         'product_nu': product_nu,
-        'grand_total', grand_total,
+        'grand_total': grand_total,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
     }
 
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def view_shopping_bag(request):
     """ A view to render shopping bag page """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add items to shopping bag """
+    qty = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    # checks if bag var in session, creates one if not
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += qty
+    else:
+        bag[item_id] = qty
+    
+    # overwrites var with session var
+    request.session['bag'] = bag
+    # redirects users
+    return redirect(redirect_url)
