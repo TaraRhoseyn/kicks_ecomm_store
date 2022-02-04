f45cc5a4f23b27f9524be5a2a1954aeae9eb9af3
Tara Rhoseyn <taragulwell@gmail.com>
2022-02-04 23:15:40 +0000

    test: Add quantity view

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,6 +11,7 @@ def bag_items(request):
     bag_items = []
     total = 0
     product_count = 0
+    delivery = Decimal(settings.DELIVERY_COST)
     bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
