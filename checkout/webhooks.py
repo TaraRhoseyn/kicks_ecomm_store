56ed166a10bb76b2429190bc8234d6dca09ceb4e
Tara Rhoseyn <taragulwell@gmail.com>
2022-02-12 13:43:17 +0000

    Add env stripe webhook secret key to app

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -12,7 +12,7 @@ import stripe
 def webhook(request):
     """Listen for webhooks from Stripe"""
     # Setup
-    wh_secret = settings.STRIPE_WH_SECRET # THIS WOULD NEED TO CHANGE TO BE 
+    wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
     # Get the webhook data and verify its signature
