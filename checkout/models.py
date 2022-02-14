ed8f63723ef82fec3efe7de0a9972fd349343d9b
Tara Rhoseyn <taragulwell@gmail.com>
2022-02-14 16:00:29 +0000

    Add foreign key for user profiles in checkout app models

--- checkout/models.py
+++ checkout/models.py
@@ -9,9 +9,12 @@ from django_countries.fields import CountryField
 from decimal import Decimal
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
