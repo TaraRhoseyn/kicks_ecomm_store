4d8a00f66ca0b63a2d613b6ffd95ed353618d328
Tara Rhoseyn <taragulwell@gmail.com>
2022-02-18 18:15:03 +0000

    Add delete_review url

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.show_all_products, name='products'),
     path('<product_id>', views.show_individual_product, name='individual_product'),
-    path('add_review/<product_id>/', views.add_review,
+    path('add_review/<int:product_id>/', views.add_review,
          name="add_review"),
-    path('edit_review/<review_id>/', views.edit_review,
+    path('edit_review/<int:review_id>/', views.edit_review,
          name="edit_review"),
+    path('delete/review/<int:review_id>/', views.delete_review,
+        name="delete_review"),
 ]
