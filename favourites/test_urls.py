9cbb43aa0f6990289a41edcb1b0d7263b095f5b2
Tara Rhoseyn <taragulwell@gmail.com>
2022-02-18 22:59:31 +0000

    tests: Add tests for favourites urls

---
+++ favourites/test_urls.py
@@ -0,0 +1,38 @@
+# IMPORTS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Third party
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+# Internal
+from .views import view_favourites, add_product_to_favourites
+from .views import remove_product_from_favourites
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class TestFavouritesUrls(SimpleTestCase):
+    """
+    Tests favourites urls are resolved
+    to the favourites views.
+    """
+
+    def test_view_favourite_url_resolves(self):
+        """
+        Checks view_favourites url is resolved with args
+        """
+        url = reverse('view_favourites')
+        self.assertEqual(resolve(url).func, view_favourites)
+    
+    def test_add_product_to_favourites_resolves(self):
+        """
+        Checks add_product_to_favourites url is resolved
+        """
+        url = reverse('add_product_to_favourites', args=['1'])
+        self.assertEqual(resolve(url).func, add_product_to_favourites)
+    
+    def test_remove_product_from_favourite_resolves(self):
+        """
+        Checks add_product_to_favourites url is resolved
+        """
+        url = reverse('remove_product_from_favourites', args=['1'])
+        self.assertEqual(resolve(url).func, remove_product_from_favourites)
