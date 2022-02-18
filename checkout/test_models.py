edf17e3a3aee5f7018e446df78a7bddb9b80a864
Tara Rhoseyn <taragulwell@gmail.com>
2022-02-18 22:37:01 +0000

    tests: Add tests for checkout app

---
+++ checkout/test_models.py
@@ -0,0 +1,50 @@
+# IMPORTS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Third party
+from django.test import TestCase
+
+# Internal
+from products.models import Product
+from .models import Order
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class TestCheckoutModels(TestCase):
+    """
+    A class for testing checkout models
+    Credit: Paul Meeneghan
+    """
+    def setUp(self):
+        """
+        Create a test product and order
+        """
+        Product.objects.create(
+            name='Test Name',
+            price='99.99',
+            colour='Test Colour',
+            code='123456',
+            description='Test Description',
+        )
+
+        Order.objects.create(
+            full_name='Test Name',
+            email='test@gmail.com',
+            phone_number='123456789',
+            country='IE',
+            town_or_city='Test City',
+            street_address1='Test Address',
+        )
+
+    def tearDown(self):
+        """
+        Delete test products and orders
+        """
+        Product.objects.all().delete()
+        Order.objects.all().delete()
+
+    def test_order_str_method(self):
+        """
+        This test tests the order number string
+        """
+        order = Order.objects.get(email='test@gmail.com')
+        self.assertEqual(str(order), order.order_number)
---
+++ checkout/test_urls.py
@@ -0,0 +1,43 @@
+# IMPORTS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Third party
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+# Internal
+from .views import checkout, checkout_success, cache_checkout_data, webhook
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class TestBrandUrls(SimpleTestCase):
+    """
+    Tests brand urls are resolved
+    to the brand views.
+    """
+    def test_brand_url_resolves(self):
+        """
+        Checks main checkout url is resolved
+        """
+        url = reverse('checkout')
+        self.assertEqual(resolve(url).func, checkout)
+
+    def test_checkout_success_url_resolves(self):
+        """
+        Checks checkout_success url is resolved with args
+        """
+        url = reverse('checkout_success', args=[1])
+        self.assertEqual(resolve(url).func, checkout_success)
+    
+    def test_cache_checkout_url_resolves(self):
+        """
+        Checks edit_brand url is resolved with args
+        """
+        url = reverse('cache_checkout_data')
+        self.assertEqual(resolve(url).func, cache_checkout_data)
+    
+    def test_webhook_url_resolves(self):
+        """
+        Checks webhook url is resolved
+        """
+        url = reverse('wh', args=['nike'])
+        self.assertEqual(resolve(url).func, webhook)
