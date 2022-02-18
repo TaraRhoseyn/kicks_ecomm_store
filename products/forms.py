1be3af005bddf0e239a41f07eaa13f5d20037d9c
Tara Rhoseyn <taragulwell@gmail.com>
2022-02-18 18:31:48 +0000

    Add ProductForm

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,13 @@
-import requests
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# IMPORTS 
+
+# Third party
 from django import forms
-from .models import Review
+
+# Internal
+from .models import Review, Product, ProductGroup, ProductType
+from brands.models import Brand
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 class RatingForm(forms.ModelForm):
     class Meta:
@@ -11,3 +18,24 @@ class RatingForm(forms.ModelForm):
             'star_rating': 'Select your rating',
             'text_review': 'Write your review'
         }
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        product_groups = ProductGroup.objects.all()
+        friendly_names = [
+            (g.id, g.get_friendly_name()) for g in product_groups]
+        product_types = ProductType.objects.all()
+        friendly_names = [
+            (t.id, t.get_friendly_name()) for t in product_types]
+        brands = Brand.objects.all()
+        friendly_names = [
+            (b.id, b.get_friendly_name()) for b in brands]
+
+        self.fields['product_groups'].choices = friendly_names
+        self.fields['product_types'].choices = friendly_names
+        self.fields['brands'].choices = friendly_names
