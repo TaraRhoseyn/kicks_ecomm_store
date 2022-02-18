5d644c4595480252ad1d6b7c7b1fc6f1c05115e5
Tara Rhoseyn <taragulwell@gmail.com>
2022-02-18 19:41:54 +0000

    Add edit_review url

--- products/urls.py
+++ products/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
      path('delete/review/<int:review_id>/', views.delete_review,
         name="delete_review"),
      path('add_product/', views.add_product, name='add_product'),
+     path('edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
