ef53bbfa647124f7cbdd0784a47b579830ae1f84
Tara Rhoseyn <taragulwell@gmail.com>
2022-02-20 18:45:08 +0000

    Change s3 region name

--- kicks/settings.py
+++ kicks/settings.py
@@ -190,12 +190,14 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'ci-ms4-kicks'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = \
+        f'{AWS_STORAGE_BUCKET_NAME}.s3.eu-west-2.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
